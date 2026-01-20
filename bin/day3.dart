4feca8fe2551a80ceaf76b61651da4549c1bc33f
Shahad <shahadzdi28@gmail.com>
2026-01-20 17:15:55 +0300

    add comments

--- bin/day3.dart
+++ bin/day3.dart
@@ -1,8 +1,14 @@
 void main() {
   print('Hello world:day3');
 }
+// git init 
+//git add
 //git status 
+// git commit
 // git log // سجل التغييرات
+
+// نغير ثم ادد ثم كومميت
+//اذا بنشوف فيه ستاتس ولوق يعلمني سجل التغييرات 
 
 // PS D:\flutter_work\day3\repo-demo\day3> git log
 // commit c6f31d65e86f35ca95662fd8ca525e957f28d2ba (HEAD -> master)
