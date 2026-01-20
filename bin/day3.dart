d8289d7c0b0c1f7e3525b56c0cb5b174ced9a9ac
Shahad <shahadzdi28@gmail.com>
2026-01-20 17:11:55 +0300

    day3 modify print

--- bin/day3.dart
+++ bin/day3.dart
@@ -1,3 +1,10 @@
 void main() {
-  print('Hello world:');
+  print('Hello world:day3');
 }
+//git status 
+// git log // سجل التغييرات
+
+// PS D:\flutter_work\day3\repo-demo\day3> git log
+// commit c6f31d65e86f35ca95662fd8ca525e957f28d2ba (HEAD -> master)
+// Author: Shahad <shahadzdi28@gmail.com>
+// Date:   Tue Jan 20 17:05:36 2026 +0300
