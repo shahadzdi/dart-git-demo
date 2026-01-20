5a1890f74b19ea0eb540dcff6efe53a152a7ea03
Shahad <shahadzdi28@gmail.com>
2026-01-20 18:05:51 +0300

    comment

--- bin/day3.dart
+++ bin/day3.dart
@@ -4,13 +4,17 @@ void main() {
 // git init 
 //git add
 //git status 
-// git commit
+// git commit -m "any msg"
 // git log // سجل التغييرات
 
 // نغير ثم ادد ثم كومميت
-//اذا بنشوف فيه ستاتس ولوق يعلمني سجل التغييرات 
+//اذا بنشوف تغير فيه شي من ستاتس, ولوق يعلمني سجل التغييرات 
+
+// عشان نربط بريموت(قيت هب) ندخل قيت هب ثم الريبوزتريز ثم نيو
 
 // PS D:\flutter_work\day3\repo-demo\day3> git log
 // commit c6f31d65e86f35ca95662fd8ca525e957f28d2ba (HEAD -> master)
 // Author: Shahad <shahadzdi28@gmail.com>
 // Date:   Tue Jan 20 17:05:36 2026 +0300
+
+
